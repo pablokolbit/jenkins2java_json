87d3d18a3888a2ad260c3a2cc88c8355715e3406
plopezgrieco <plopezgrieco@gmail.com>
2025-01-08 12:39:31 +0100

    pom2 y Jorgelina

--- json1/src/main/java/es/kolbit/json1/Main.java
+++ json1/src/main/java/es/kolbit/json1/Main.java
@@ -3,7 +3,7 @@ package es.kolbit.json1;
 public class Main {
 
 	public static void main(String[] args) {
-		String json = "{\"nombre\":\"Mary\"}";
+		String json = "{\"nombre\":\"Jorgelina\"}";
 		LeerJsonPersona leer = new LeerJsonPersona();
 		Persona p = leer.getPersona(json);
 		System.out.println(p);

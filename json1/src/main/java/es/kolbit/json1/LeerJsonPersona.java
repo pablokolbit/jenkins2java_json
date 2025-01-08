a8e2bed61ac6a98fae59eeeab497156877f466eb
plopezgrieco <plopezgrieco@gmail.com>
2025-01-08 11:57:07 +0100

    inical

---
+++ json1/src/main/java/es/kolbit/json1/LeerJsonPersona.java
@@ -0,0 +1,20 @@
+package es.kolbit.json1;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class LeerJsonPersona {
+
+	public Persona getPersona(String json) {
+		
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode nodo = null;
+		try {
+			nodo = mapper.readTree(json);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return new Persona(nodo.get("nombre").asText());
+	}
+}
---
+++ json1/src/main/java/es/kolbit/json1/Main.java
@@ -0,0 +1,12 @@
+package es.kolbit.json1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String json = "{\"nombre\":\"Mary\"}";
+		LeerJsonPersona leer = new LeerJsonPersona();
+		Persona p = leer.getPersona(json);
+		System.out.println(p);
+	}
+
+}
